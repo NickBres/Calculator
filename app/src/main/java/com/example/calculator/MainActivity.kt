22c2e95465fb379dfab9ec89b58a3e680a8a18d8
NickBres <70432147+NickBres@users.noreply.github.com>
2023-08-13 17:47:58 +0300

    Design

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        // TODO
+        Toast.makeText(this,"Digit pressed", Toast.LENGTH_SHORT).show()
+    }
+}
